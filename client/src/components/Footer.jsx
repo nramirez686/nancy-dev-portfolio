32219775a075815a43218269d3376d1c499810ff
Fancy Nancy <ramireznancy.686@gmail.com>
2023-12-03 22:35:49 -0700

    fixed footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 const Footer = () => {
   return (
-    <footer>
-      <p>&copy; {new Date().getFullYear()} Nancy's Portfolio </p>
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="social-links">
+          <a href="https://github.com/nramirez686" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a href="https://www.linkedin.com/in/nancy-ramirez-460a62264/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://stackoverflow.com/users/21980715/nancy-ramirez" target="_blank" rel="noopener noreferrer">Stack Overflow</a>
+        </div>
+      </div>
     </footer>
   );
 };
